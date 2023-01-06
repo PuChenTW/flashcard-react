462f6f809474280c893d4f1e11613db6c67f223e
Pu Chen <puchen.tw@gmail.com>
2023-01-06 11:44:08 +0800

    style: Add formater.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { useState } from 'react';
-import Card from './components/Card';
-import Toolbar from './components/Toolbar';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import { useFlashcards } from './hooks/useFlashcards';
+import "./App.css";
+import { useState } from "react";
+import Card from "./components/Card";
+import Toolbar from "./components/Toolbar";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { useFlashcards } from "./hooks/useFlashcards";
 
 function App() {
   const [allCards] = useFlashcards();
@@ -12,37 +12,47 @@ function App() {
   const [curCardId, setCurCardId] = useState(1);
 
   let goToPrev = () => {
-    if ( isValidId(curCardId - 1)) {
+    if (isValidId(curCardId - 1)) {
       setCurCardId(curCardId - 1);
-    }
-    else {
+    } else {
       setCurCardId(totalCards);
     }
-  }
+  };
 
   let goToNext = () => {
-    if ( isValidId(curCardId + 1)) {
+    if (isValidId(curCardId + 1)) {
       setCurCardId(curCardId + 1);
-    }
-    else {
+    } else {
       setCurCardId(1);
     }
   };
 
   function isValidId(id) {
-    return ( id <= totalCards && id >= 1);
+    return id <= totalCards && id >= 1;
   }
 
   return (
     <>
-    <Toolbar></Toolbar>
-    <Container className="container-main">
-      <Card {...allCards.find(item=> item.id === curCardId)}></Card>
-      <div className="action-buttons">
-        <Button variant="warning" className="me-4 prev-next-btns" onClick={goToPrev}>Prev</Button>
-        <Button variant="success" className="ms-4 prev-next-btns" onClick={goToNext}>Next</Button>
-      </div>
-    </Container>
+      <Toolbar></Toolbar>
+      <Container className="container-main">
+        <Card {...allCards.find((item) => item.id === curCardId)}></Card>
+        <div className="action-buttons">
+          <Button
+            variant="warning"
+            className="me-4 prev-next-btns"
+            onClick={goToPrev}
+          >
+            Prev
+          </Button>
+          <Button
+            variant="success"
+            className="ms-4 prev-next-btns"
+            onClick={goToNext}
+          >
+            Next
+          </Button>
+        </div>
+      </Container>
     </>
   );
 }
